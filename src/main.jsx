057421ca007c114b97e562c33bab77c4ed5c100b
SoMedNinja <Robbanh94@gmail.com>
2026-01-22 21:40:33 +0100

    Add error boundary to avoid white screen

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import ErrorBoundary from "./Components/ErrorBoundary.jsx";
 
 if (typeof globalThis !== "undefined" && !globalThis.players) {
   globalThis.players = [];
@@ -9,6 +10,8 @@ if (typeof globalThis !== "undefined" && !globalThis.players) {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
