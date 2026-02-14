f35d7f97663142a895b0c25401e89f3d9624faaf
SoMedNinja <Robbanh94@gmail.com>
2026-02-14 10:01:59 +0100

    Fix iOS pull-to-refresh showing native spinner

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -461,6 +461,8 @@ export default function SchedulePage() {
 
   return (
     <PullToRefresh
+      // Note for non-coders: this class lets us apply iOS-specific CSS so only our custom refresh animation is shown.
+      className="app-pull-to-refresh"
       onRefresh={handleRefresh}
       pullingContent={<PullingContent />}
       refreshingContent={<RefreshingContent />}
